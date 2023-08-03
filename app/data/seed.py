a6d3f516bc3f43ca6c9f41692e12d5d688abdcaa
bunchhun <chhim.bunchhun@rupp.edu.kh>
2023-08-03 16:21:16 +0700

    Add profile data

--- app/data/seed.py
+++ app/data/seed.py
@@ -1,15 +1,28 @@
 from app.models import *
 
 def insert_menu():
-    menus=[Menu(name_latin='Setting',order=1),Menu(name_latin='Student',order=2)]
+    menus = [
+        Menu(name_latin='Setting',order=1),\
+        Menu(name_latin='Student',order=2)
+    ]
     db.session.add_all(menus)
     db.session.commit()
     
 def insert_sub_menu():
-    sub_menus=[SubMenu(menu_id=1,name_latin='User',order=1,end_point='e01'),\
-            SubMenu(menu_id=1,name_latin='Password',order=2,end_point='e02'),\
-            SubMenu(menu_id=2,name_latin='Dashboard',order=1,end_point='e03'),\
-            SubMenu(menu_id=2,name_latin='Course',order=2,end_point='e04')
-        ]
+    sub_menus = [
+        SubMenu(menu_id=1,name_latin='User',order=1,end_point='e01'),\
+        SubMenu(menu_id=1,name_latin='Password',order=2,end_point='e02'),\
+        SubMenu(menu_id=2,name_latin='Dashboard',order=1,end_point='e03'),\
+        SubMenu(menu_id=2,name_latin='Course',order=2,end_point='e04')
+    ]
     db.session.add_all(sub_menus)
+    db.session.commit()
+    
+def insert_profile():
+    profiles = [
+        Profile(acronym='AD',name_latin='Administrator'),\
+        Profile(acronym='ST',name_latin='Student'),\
+        Profile(acronym='TE',name_latin='Teacher')
+    ]
+    db.session.add_all(profiles)
     db.session.commit()
