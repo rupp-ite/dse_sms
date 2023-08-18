eb33c84700334b2f28b4497e812ce8e2bbc50b86
bunchhun <chhim.bunchhun@rupp.edu.kh>
2023-08-18 16:22:44 +0700

    Adding api

--- app/api/test.py
+++ app/api/test.py
@@ -2,7 +2,7 @@ from flask_restx import Namespace, Resource
 
 ns_test = Namespace('test', description='Testing API', path='/test')
 
-@ns_test.doc(responses={200: 'Success',400: 'Validation Error'}, description='This is the description of Test1 API')
+@ns_test.doc(responses={200: 'Success',400: 'Bad request', 401: 'Unauthorized'}, description='Description of Hello API')
 @ns_test.route('/hello')
 class Hello(Resource):
     def get(self):
